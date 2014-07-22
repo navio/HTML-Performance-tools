e732a40e7c9bfe0d5954af324db292c64624e2c9
Alberto Navarro <alnav@amazon.com>
2014-07-22 12:35:15 -0400

    Adding elements descriptions

--- time.js
+++ time.js
@@ -16,12 +16,32 @@ var page = document.location.pathname;
 var pt = window.performance.timing;
 var sh = [];
 
+/*
+Returns the time range from clicking a link on the previosu page, to the time after load event completitions.
+Clicking on a link includes time of previous page unload.
+*/
 var transitionTime = pt.domContentLoadedEventEnd - pt.navigationStart;
+/*
+Returns the time just before server connection begins to the time just before document readiness reach completness.
+*/
 
 var domLoadCompeted = pt.domComplete - pt.connectStart;
 
+/*
+Returns the time that takes for elements to get loaded in the DOM.
+
+The range goes from:
+the time just before DOMContentLoaded starts, 
+to the time after DOMContentLoaded event is complet. 
+*/
 var domContentLoad = pt.domContentLoadedEventEnd - pt.domContentLoadedEventStart;
 
+/*
+Returns the time that takes to look up for the domain
+*/
 var domainLookUp = pt.domainLookupEnd - pt.domainLookupStart;
 
+/*
+Returns the time that takes to run the load event.
+*/
 var loadEvent = pt.loadEventEnd - pt.loadEventStart;
